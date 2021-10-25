0f50d65942c95f3826c748ce86396c66879a0b9d
Ravandhika <youravandhika123@gmail.com>
2021-10-25 12:16:09 +0700

    Layout..

---
+++ app/src/main/java/com/example/mymoneybook/TambahPemasukanActivity.java
@@ -0,0 +1,14 @@
+package com.example.mymoneybook;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class TambahPemasukanActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_pemasukan);
+    }
+}
